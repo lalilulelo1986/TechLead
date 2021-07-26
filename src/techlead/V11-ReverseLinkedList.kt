b53ba0433ba28bc9414c0b61eac1fe3f7d8cefc9
kmax <lalilulelo1986@gmail.com>
2021-07-27 01:04:33 +0600

    tuts

--- src/techlead/V11-ReverseLinkedList.kt
+++ src/techlead/V11-ReverseLinkedList.kt
@@ -3,12 +3,13 @@ package techlead
 import java.util.*
 
 fun main() {
-    val ll = LinkedList<Int>().apply {
-        add(1); add(2); add(3); add(4); add(5)
-    }
-    ReverseLinkedList().reverse(ll)
+//    val ll = LinkedList<Int>().apply {
+//        add(1); add(2); add(3); add(4); add(5)
+//    }
+//    ReverseLinkedList().reverse(ll)
 
-    val linkedList = LinkedNode(6).also { it.next = LinkedNode(4).also { it.next = LinkedNode(2) } }
+    val linkedList = LinkedNode(1).also { it.next = LinkedNode(2).also { it.next = LinkedNode(3) } }
+    println(linkedList)
     ReverseLinkedList2().reverse(linkedList)
 
 }
@@ -24,8 +25,17 @@ class ReverseLinkedList {
     }
 }
 
+// GOOD. with no new list
 class ReverseLinkedList2 {
-    fun reverse(linkedList: LinkedNode<Int>) {
-        for ()
+    fun reverse(linkedList: LinkedNode<Int>?) {
+        var previous: LinkedNode<Int>? = null
+        var current = linkedList
+        while (current != null) {
+            val next = current.next
+            current.next = previous
+            previous = current
+            current = next
+        }
+        println(previous)
     }
 }
