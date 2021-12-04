698fedcf971b8ce4ae378622038e38bae4e8aa65
kmax <lalilulelo1986@gmail.com>
2021-12-04 15:06:01 +0600

    new

---
+++ src/techlead/V51-CircleOfChainedWords.kt
@@ -0,0 +1,11 @@
+package techlead
+
+//print(chainedWords(['apple', 'eggs', 'snack', 'karat', 'tuna']))
+//# True
+//
+//print(chainedWords(['apple', 'eggs', 'snack', 'karat', 'tunax']))
+//# False
+
+fun main() {
+    val arr = arrayOf("apple", "eggs", "snack", "karat", "tuna")
+}
