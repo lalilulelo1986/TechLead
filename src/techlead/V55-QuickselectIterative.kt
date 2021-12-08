d9ba2092f53ec3d5993473eebeafedd5a9b22830
kmax <lalilulelo1986@gmail.com>
2021-12-08 23:29:30 +0600

    new

--- src/techlead/V55-QuickselectIterative.kt
+++ src/techlead/V55-QuickselectIterative.kt
@@ -1,12 +1,20 @@
 package techlead
 
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     val arr = arrayOf(4, 3, 5, 2, 0, 1)
+    println("heap largest: ${findKthLargest(arr, 3)}")
     println(quickSelect(arr, 3))
     val arr2 = arrayOf(8, 7, 2, 3, 4, 1, 5, 6, 9, 0)
+    println("heap largest: ${findKthLargest(arr2, 4)}")
     println(quickSelect(arr2, 3))
 }
 
+// My fast
 fun quickSelect(array: Array<Int>, k: Int): Int {
     var partStart = 0
     var partEnd = array.size - 1
@@ -27,7 +35,7 @@ fun quickSelect(array: Array<Int>, k: Int): Int {
             }
             p2++
         }
-        println("partStart: $partStart, partEnd: $partEnd, p1: $p1, p2: $p2, pivot: $pivot")
+//        println("partStart: $partStart, partEnd: $partEnd, p1: $p1, p2: $p2, pivot: $pivot")
         tmp = array[pivot]
         array[pivot] = array[p1]
         array[p1] = tmp
@@ -46,8 +54,76 @@ fun quickSelect(array: Array<Int>, k: Int): Int {
         }
         if (partStart == partEnd)
             return array[p1]
-        println(array.toList())
+//        println(array.toList())
     }
 
     return 0
 }
+
+// My heap
+fun findKthLargest(array: Array<Int>, k: Int): Int? {
+    val heap = PriorityQueue<Int> { f, s -> s - f }
+    array.forEach { heap.add(it) }
+    for (s in 1 until k)
+        heap.poll()
+    return heap.poll()
+}
+
+/* TechLead
+import heapq
+
+
+def findKthLargest(arr, k):
+  for i in range(0, k):
+    (max_value, max_index) = (arr[0], 0)
+    for j in range(0, len(arr)):
+      if max_value < arr[j]:
+        (max_value, max_index) = arr[j], j
+    arr = arr[:max_index] + arr[max_index + 1:]
+  for j in range(0, len(arr)):
+    if max_value < arr[j]:
+      (max_value, max_index) = arr[j], j
+  return max_value
+
+
+def findKthLargest2(arr, k):
+  return sorted(arr)[-k]
+
+
+def findKthLargest2(arr, k):
+  arr = list(map(lambda x: -x, arr))
+  heapq.heapify(arr)
+  for i in range(0, k - 1):
+    heapq.heappop(arr)
+  return -arr[0]
+
+
+def partition(arr, low, high):
+  pivot = arr[high]
+  i = low
+  for j in range(low, high):
+    if arr[j] <= pivot:
+      arr[i], arr[j] = arr[j], arr[i]
+      i += 1
+  arr[i], arr[high] = arr[high], arr[i]
+  return i
+
+
+def quickselect(arr, k):
+  k = len(arr) - k
+  left = 0
+  right = len(arr) - 1
+
+  while left <= right:
+    pivotIndex = partition(arr, left, right)
+    if pivotIndex == k:
+      return arr[pivotIndex]
+    elif pivotIndex > k:
+      right = pivotIndex - 1
+    else:
+      left = pivotIndex + 1
+  return -1
+
+
+print(quickselect([8, 7, 2, 3, 4, 1, 5, 6, 9, 0], 3))
+ */
