65d58631f3b8850b85436b0bfc3b59de9ff5d4eb
kmax <lalilulelo1986@gmail.com>
2022-10-20 13:09:38 +0600

    noveo

--- src/techlead/noveo/18-trapping-rain-water.kt
+++ src/techlead/noveo/18-trapping-rain-water.kt
@@ -5,6 +5,9 @@ import kotlin.test.assertEquals
 fun main() {
     val array = arrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1).toIntArray()
     assertEquals(6, trap(array))
+
+    val array2 = arrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1).toIntArray()
+    println(trap2(array2))
 }
 
 fun trap(height: IntArray): Int {
@@ -27,4 +30,26 @@ fun trap(height: IntArray): Int {
     }
 
     return result
+}
+
+// линейное время. конст. память
+fun trap2(height: IntArray): Int {
+    val maxIndex = height.maxOrNull()?.let { height.indexOf(it) } ?: -1
+
+    var max = 0
+    var size = 0
+    for (i in 0..maxIndex) {
+        if (max > height[i])
+            size += max - height[i]
+        if (height[i] > max)
+            max = height[i]
+    }
+    max = 0
+    for (i in height.size - 1 downTo maxIndex) {
+        if (max > height[i])
+            size += max - height[i]
+        if (height[i] > max)
+            max = height[i]
+    }
+    return size
 }
